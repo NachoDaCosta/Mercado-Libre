c5b64ee3516fc6963ad478829bce4d7198fecfff
NachoDaCosta <nacho5981@gmail.com>
2023-03-02 13:52:25 -0300

    arreglo de css de main, quité los paddings

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -37,7 +37,65 @@ const Products=()=>{
 
 
 
-            <div>2</div>
+            <div className="products-with-pictures">
+                <div className="visita-text">
+                    <div>Basado en tu ultima visita</div>
+                    <div className="see-more-history">Ver más</div>
+                </div>
+
+                <div className="products-history">
+                    <div className="individual-product-history">
+                        <div>
+                            <img alt="" src="https://http2.mlstatic.com/D_Q_NP_626789-MLU51824281692_102022-AB.webp"/>
+                        </div>
+                        <div className="prices">
+                            <div className="price">$300</div>
+                            <div className="green-taxes">10x $30 sin interés</div>
+                        </div>
+                    </div>
+
+                    <div className="individual-product-history">
+                        <div>
+                            <img alt="" src="https://http2.mlstatic.com/D_Q_NP_695307-MLU31698123822_082019-AB.webp"/>
+                        </div>
+                        <div className="prices">
+                            <div className="price">$300</div>
+                            <div className="green-taxes">10x $30 sin interés</div>
+                        </div>
+                    </div>
+
+                    <div className="individual-product-history">
+                        <div>
+                            <img alt="" src="https://http2.mlstatic.com/D_Q_NP_803818-MLU31241430495_062019-AB.webp"/>
+                        </div>
+                        <div className="prices">
+                            <div className="price">$300</div>
+                            <div className="green-taxes">10x $30 sin interés</div>
+                        </div>
+                    </div>
+
+                    <div className="individual-product-history">
+                        <div>
+                            <img alt="" src="https://http2.mlstatic.com/D_Q_NP_698793-MLU31244308516_062019-AB.webp"/>
+                        </div>
+                        <div className="prices">
+                            <div className="price">$1200</div>
+                            <div className="green-taxes">10x $120 sin interés</div>
+                        </div>
+                    </div>
+
+                    <div className="individual-product-history">
+                        <div>
+                            <img alt="" src="https://http2.mlstatic.com/D_Q_NP_640932-MLU49855811548_052022-AB.webp"/>
+                        </div>
+                        <div className="prices">
+                            <div className="price">$600</div>
+                            <div className="green-taxes">10x $60 sin interés</div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
